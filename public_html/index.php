c7fc71ab34c6b2240112bb5fb0a035e7403c1a24
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2019-12-28 00:58:21 +0600

    Basic Setup 2

--- public_html/index.php
+++ public_html/index.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Inventory System</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
     
+    <div class="jumbotron">
+       <h1>Hello World</h1>
+    </div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="./js/main.js"></script>
+
 </body>
 </html>
